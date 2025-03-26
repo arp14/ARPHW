37ec94f00d90d7d561ec27da0d3368f5e673fd1d
Alan R Peslak <arp14@psu.edu>
2025-03-26 15:19:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,41 @@
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('hellopwa').then(function (cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/style.css',
-        '/icon.png'
-      ]);
+const CACHE_NAME = 'hellopwa-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './style.css',
+  './icon.png',
+  './'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
-});
